dcdc4b4c77816ac612a3c27d4467eaeb66b008d4
ismadia09 <ismadia09@gmail.com>
2018-05-26 14:07:26 +0200

    clean project

---
+++ src/providers/spacex-api/spacex-api.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Launch } from '../../app/Models/Launch';
+
+
+/*
+  Generated class for the SpacexApiProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class SpacexApiProvider {
+
+  baseUrl = "https://api.spacexdata.com/v2";
+  constructor(private http: HttpClient) {
+    
+  }
+
+  getAllLaunches() : Observable<Launch[]>{
+    const endPointUrl = `${this.baseUrl}/launches/all`;
+   return this.http.get<Launch[]>(endPointUrl)
+  }
+
+}
